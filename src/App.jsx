c78b7442b9050896c8e8fce69056e1558ee0a489
unknown <youyu00022@gmail.com>
2024-06-02 18:25:53 -0500

    updated Contact.jsx

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import PortfolioContainer from "./components/PortfolioContainer";
+import Header from "./components/Header";
+
+const App = () => {
+return (
+    <>
+{/* <Header /> */}
+    
+<PortfolioContainer />
+    </>
+)
+}
+export default App;
---
+++ src/components/pages/Contact.jsx
@@ -0,0 +1,14 @@
+export default function Contact() {
+  return (
+    <div>
+      <h1>Contact</h1>
+      <h2>Location</h2>
+      <p>St. Paul, Minnesota</p>
+        <h2>Phone Number</h2>
+      <p>(651)-278-9506</p>
+      <h2>Email</h2>
+      <p>youyu00022@gmail.com</p>
+      
+    </div>
+  );
+}
